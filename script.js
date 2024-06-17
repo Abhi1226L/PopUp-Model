bcb113d53dd434cc89d8c1e2eda64751d24e18fa
Abhishek Parida <131433736+Abhi1226L@users.noreply.github.com>
2024-06-18 01:15:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let btn = document.querySelector("button");
 let popup = document.querySelector(".popup");
+let modelContainer = document.querySelector("#model-container");
 let icon = document.querySelector(".icon");
 btn.addEventListener("click",function(){
-    popup.style.display = "block";
+    modelContainer.style.display = "block";
 })
 
 icon.addEventListener("click",function(){
-    popup.style.display = "none";
+    modelContainer.style.display = "none";
 })
